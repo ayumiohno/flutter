488c2d264e7eecfc25e2f23fc8646a7aa9ea7bd6
ayumiohno <ayumi0130ohno@gmail.com>
2024-08-25 19:29:55 +0900

    Add Background grid and icons

--- lib/background.dart
+++ lib/background.dart
@@ -7,26 +7,42 @@ class Background extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
+      final linesX = 17;
+      double gridSpace = constraints.maxWidth / linesX;
+      final linesY = (constraints.maxHeight / gridSpace).round();
+      var h =Container(width: 1.61, height: constraints.maxHeight, color: Colors.white);
+      var v =Container(width: constraints.maxWidth, height: 1.61, color: Colors.white);
       return Container(
+        color: Color(0xFFF2F3FE),
         child: Stack(
           children: [
+            ...List.generate(linesX, (index) => Positioned(left: index * gridSpace, child: h)),
+            ...List.generate(linesY, (index) => Positioned(top: index * gridSpace, child: v)),
             Positioned(
-                top: constraints.maxHeight * 0.05,
-                right: constraints.maxWidth * 0.05,
-                child: Image.asset(
-                  'assets/star.png',
-                  width: constraints.maxWidth * 0.25,
-                  height: constraints.maxWidth * 0.25,
-                  fit: BoxFit.cover,
-                )),
+                top: 10,
+                right: 5,
+                child: Transform.rotate(
+                    angle: 0.96,
+                    child: Image.asset(
+                      'assets/star.png',
+                      width: 85.19,
+                      fit: BoxFit.cover,
+                    ))),
+            Positioned(
+              left: 0,
+              bottom: 0,
+              child: Image.asset(
+                'assets/path.png',
+                width: 240,
+                fit: BoxFit.cover,
+              ),
+            ),
             Align(
               alignment: Alignment.center,
-              // top: constraints.maxHeight * 0.05,
-              // left: constraints.maxWidth * 0.08,
               child: Transform.rotate(
-                angle: -0.174533, // -10 degrees in radians
+                angle: -0.174533,
                 child: Container(
-                  width: constraints.maxWidth * 0.83,
+                  width: constraints.maxWidth * 0.85,
                   child: frame,
                 ),
               ),
