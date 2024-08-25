b020604123703c281993262734a0d7768cde7b31
ayumiohno <ayumi0130ohno@gmail.com>
2024-08-25 14:23:43 +0900

    Support frame1

--- lib/frame.dart
+++ lib/frame.dart
@@ -8,77 +8,96 @@ class ShareFrame extends StatelessWidget {
         double containerPadding = constraints.maxWidth * 0.05;
         return Center(
           child: Container(
-            padding: EdgeInsets.all(containerPadding),
-            color: Colors.blue[50],
+            color: Colors.white,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 Stack(
                   children: [
                     // Family image
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.asset(
-                        'assets/image.png', // Replace with your image path
-                        width: double.infinity,
-                        fit: BoxFit.cover,
+                    Padding(
+                      padding: EdgeInsets.all(containerPadding),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.asset(
+                          'assets/image.png', // Replace with your image path
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                     // Top-left icon
                     Positioned(
-                      top: containerPadding * 0.5,
-                      left: containerPadding * 0.5,
-                      child: Icon(
-                        Icons.pets,
-                        size: constraints.maxWidth * 0.1,
-                        color: Colors.white,
+                      top: containerPadding * 0.05,
+                      left: containerPadding * 0.05,
+                      child: Image.asset(
+                        'assets/icon2.png',
+                        width: constraints.maxWidth * 0.2,
+                        height: constraints.maxWidth * 0.2,
                       ),
                     ),
                     // Bottom-right icon
                     Positioned(
-                      bottom: containerPadding * 0.5,
-                      right: containerPadding * 0.5,
-                      child: Icon(
-                        Icons.pets,
-                        size: constraints.maxWidth * 0.1,
-                        color: Colors.white,
+                      bottom: containerPadding * 0.1,
+                      right: containerPadding * 0.1,
+                      child: Image.asset(
+                        'assets/icon1.png',
+                        width: constraints.maxWidth * 0.2,
+                        height: constraints.maxWidth * 0.2,
                       ),
                     ),
                   ],
                 ),
-                SizedBox(height: containerPadding * 0.5),
                 // Product info
-                Row(
-                  children: [
-                    ClipOval(
-                      child: Image.asset(
-                        'assets/image.png', // Replace with your icon path
-                        width: constraints.maxWidth * 0.1,
-                        height: constraints.maxWidth * 0.1,
-                        fit: BoxFit.cover,
+                Container(
+                  padding: EdgeInsets.only(
+                    left: containerPadding,
+                    right: containerPadding,
+                    bottom: containerPadding,
+                  ),
+                  child: Row(
+                    children: [
+                      Stack(children: [
+                        Padding(
+                          padding: EdgeInsets.all(containerPadding * 0.5),
+                          child: Image.asset(
+                            'assets/product.png',
+                            width: constraints.maxWidth * 0.25,
+                            height: constraints.maxWidth * 0.25,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Positioned(
+                            bottom: 0,
+                            left: 0,
+                            child: Image.asset(
+                              'assets/logo.png',
+                              width: constraints.maxWidth * 0.1,
+                              height: constraints.maxWidth * 0.1,
+                            )),
+                      ]),
+                      SizedBox(width: containerPadding),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Product Name',
+                            style: TextStyle(
+                              fontSize: constraints.maxWidth * 0.05,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '26.08.2024',
+                            style: TextStyle(
+                              fontSize: constraints.maxWidth * 0.04,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    SizedBox(width: containerPadding * 0.5),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Product Name',
-                          style: TextStyle(
-                            fontSize: constraints.maxWidth * 0.05,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          '26.08.2024',
-                          style: TextStyle(
-                            fontSize: constraints.maxWidth * 0.04,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ],
             ),
